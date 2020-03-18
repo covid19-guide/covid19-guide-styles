2d747da21bfb7f2b31fb5cdd7c98297ccee7a0c0
Ted Han <ted@knowtheory.net>
2020-03-17 23:47:49 -0700

    Add google analytics.

--- userAuth.js
+++ userAuth.js
@@ -5,10 +5,8 @@ const router = require('express-promise-router')()
 
 router.use(async (req, res) => {
   req.userInfo = {
-    email: 'demo.user@example.com',
-    userId: '10',
     // prevents incorrect GA tracking
-    analyticsUserId: false
+    analyticsUserId: 'UA-161062577-1'
   }
   
   return 'next'
